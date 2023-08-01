4c2216f775f8c691276a070d88c9884409d2aa49
pemponce <a.zheltuhin@mail.ru>
2023-08-01 19:30:57 +0300

    init commit

---
+++ src/main/java/artem/BotApl.java
@@ -0,0 +1,11 @@
+package artem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotApl {
+    public static void main(String[] args) {
+        SpringApplication.run(BotApl.class, args);
+    }
+}
