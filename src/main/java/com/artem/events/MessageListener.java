d0d0bf6268d29e5a915a22b1bbabdba3cb31f86c
pemponce <a.zheltuhin@mail.ru>
2023-08-04 15:29:55 +0300

    init

--- src/main/java/com/artem/events/MessageListener.java
+++ src/main/java/com/artem/events/MessageListener.java
@@ -1,6 +1,8 @@
 package com.artem.events;
 
+import discord4j.core.object.entity.Member;
 import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.VoiceChannel;
 import reactor.core.publisher.Mono;
 
 import discord4j.core.object.entity.User;
@@ -9,20 +11,30 @@ import discord4j.core.object.entity.User;
 public abstract class MessageListener {
 
     public Mono<Void> processCommand(Message eventMessage) {
-        return Mono.just(eventMessage)
-                .doOnNext(message -> {
-                    System.out.println("Получено сообщение: " + message.getContent());
-                    System.out.println("Автор: " + message.getAuthor().map(User::getUsername).orElse("Неизвестно"));
-                })
-                .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
-                .filter(message -> message.getContent().equalsIgnoreCase("!todo"))
-                .flatMap(Message::getChannel)
-                .flatMap(channel -> channel.createMessage("Планы на сегодня:\n" +
+
+        return Mono.just(eventMessage).doOnNext(message -> {
+            System.out.println("Получено сообщение: " + message.getContent());
+            System.out.println("Автор: " + message.getAuthor().map(User::getUsername).orElse("Неизвестно"));
+        }).filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false)).flatMap(message -> {
+            if (message.getContent().equalsIgnoreCase("!todo")) {
+                return message.getChannel().flatMap(channel -> channel.createMessage("Планы на сегодня:\n" +
                         " - написать бота\n" +
-                        " - пообедать\n" +
-                        " - поиграть в игру\n" +
-                        "@mononoawaree "))
-                .doOnNext(response -> System.out.println("Ответ: " + response))
-                .then();
+                        " - подрочить\n" +
+                        " - поспать\n" +
+                        "@mononoawaree")).doOnNext(response -> System.out.println("Ответ: " + response)).then();
+            } else if (message.getContent().equalsIgnoreCase("!help")) {
+                return message.getChannel().flatMap(channel -> channel.
+                                createMessage("чтобы запустить ботика напишите !p название песни"))
+                        .doOnNext(response -> System.out.println("Ответ: " + response)).then();
+            } else if (message.getContent().startsWith("!p ")) {
+                String songName = message.getContent().substring(3);
+
+                return (message.getChannel().flatMap(channel -> channel.createMessage("!p " + songName)))
+                        .doOnNext(response -> System.out.println("Ответ: " + response))
+                        .then();
+            } else {
+                return Mono.empty(); // Если ни одна из команд не совпала, возвращаем пустой Mono
+            }
+        });
     }
 }
