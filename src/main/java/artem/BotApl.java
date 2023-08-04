091c5ee3f2c949d5d974974892523fb615f108dc
VallessGIT <vadim.nigm@yandex.ru>
2023-08-04 13:06:38 +0300

    Added context singleton ref

--- src/main/java/artem/BotApl.java
+++ src/main/java/artem/BotApl.java
@@ -1,11 +1,17 @@
 package artem;
 
+import lombok.Getter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BotApl {
+
+    @Getter
+    private static ConfigurableApplicationContext context;
+
     public static void main(String[] args) {
-        SpringApplication.run(BotApl.class, args);
+        context =  SpringApplication.run(BotApl.class, args);
     }
 }
